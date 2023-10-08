be533ed2b89ff43d7fc1f75ac92f298f391ca331
varikakasandor <varikakasandor@gmail.com>
2023-10-08 14:12:12 +0200

    Training plot and legend added

--- final_comparison_experiment/pipeline.py
+++ final_comparison_experiment/pipeline.py
@@ -10,7 +10,8 @@ from models import SimpleNN
 class FairnessAwareLearningExperiment:
     def __init__(self, data, fairness_metric, fairness_name, dataset_name, fairness_weights, analysis_metric,
                  print_progress=True):
-        self.x_train, self.y_train, self.a_train, self.x_test, self.y_test, self.a_test = data
+        x_train, y_train, a_train, x_test, y_test, a_test = data
+        self.x_train, self.y_train, self.a_train, self.x_test, self.y_test, self.a_test = torch.tensor(x_train.astype(np.float32)), torch.tensor(y_train.astype(np.float32)), torch.tensor(a_train.astype(np.float32)), torch.tensor(x_test.astype(np.float32)), torch.tensor(y_test.astype(np.float32)), torch.tensor(a_test.astype(np.float32))
         self.fairness_metric = fairness_metric
         self.fairness_name = fairness_name
         self.dataset_name = dataset_name
@@ -19,10 +20,7 @@ class FairnessAwareLearningExperiment:
         self.analysis_metric = analysis_metric
 
     def train_model(self, model, fairness_weight=1.0, lr=1e-5, num_epochs=100):
-        X = torch.tensor(self.x_train.astype(np.float32))
-        A = torch.tensor(self.a_train.astype(np.float32))
-        Y = torch.tensor(self.y_train.astype(np.float32))
-        dataset = data_utils.TensorDataset(X, Y, A)
+        dataset = data_utils.TensorDataset(self.x_train, self.y_train, self.a_train)
         dataset_loader = data_utils.DataLoader(dataset=dataset, batch_size=200, shuffle=True)
 
         data_fitting_loss = nn.BCELoss()
@@ -30,7 +28,7 @@ class FairnessAwareLearningExperiment:
 
         for j in range(num_epochs):
             if self.print_progress:
-                print(f"EPOCH {j} started")
+                print(f"EPOCH {j + 1} started")
             for i, (x, y, a) in enumerate(dataset_loader):
                 def closure():
                     optimizer.zero_grad()
@@ -50,40 +48,40 @@ class FairnessAwareLearningExperiment:
                     f"TRAIN -- loss: {bce_curr_train}, nd: {nd_curr_train}, combined: {bce_curr_train + fairness_weight * nd_curr_train}")
 
     def evaluate(self, model, dataset="test"):
-        if dataset == "train":
-            X, A, Y = torch.tensor(self.x_train.astype(np.float32)), torch.Tensor(
-                self.a_train.astype(np.float32)), torch.tensor(self.y_train.astype(np.float32))
-        elif dataset == "test":
-            X, A, Y = torch.tensor(self.x_test.astype(np.float32)), torch.Tensor(
-                self.a_test.astype(np.float32)), torch.tensor(self.y_test.astype(np.float32))
-        prediction = model(X).detach().flatten()
-        loss = nn.BCELoss()(prediction, Y)
-        nd_loss = torch.max(self.fairness_metric(prediction, A, Y))
+        x, a, y = (self.x_train, self.a_train, self.y_train) if dataset == "train" else (self.x_test, self.a_test, self.y_test)
+        prediction = model(x).detach().flatten()
+        loss = nn.BCELoss()(prediction, y)
+        nd_loss = torch.max(self.fairness_metric(prediction, a, y))
         return loss.item(), nd_loss
 
     def run_analysis(self):
-        X, A, Y = torch.tensor(self.x_test.astype(np.float32)), torch.Tensor(
-            self.a_test.astype(np.float32)), torch.tensor(self.y_test.astype(np.float32))
-        objective_losses = []
-        nd_losses = []
+        objective_losses_train, objective_losses_test = [], []
+        nd_losses_train, nd_losses_test = [], []
         categories = None
-        bottlenecks = []
+        bottlenecks_train, bottlenecks_test = [], []
         for fairness_weight in self.fairness_weights:
             if self.print_progress:
                 print(f"Fairness weight {fairness_weight} started")
             model = SimpleNN(self.x_train.shape[1], 1)
             self.train_model(model, fairness_weight=fairness_weight)
-            prediction = model(X).detach().flatten()
-            loss = nn.BCELoss()(prediction, Y)
-            curr_fairness_losses, categories = self.analysis_metric(prediction, A, Y)  # categories is the same for all iterations, should be restructured
-            curr_bottleneck = ["green"] * len(curr_fairness_losses)
-            curr_bottleneck[curr_fairness_losses.index(max(curr_fairness_losses))] = "red"
-            bottlenecks.append(curr_bottleneck)
-            objective_losses.append(loss)
-            nd_losses.append(curr_fairness_losses)
-        objective_losses, nd_losses = np.array(objective_losses), np.array(nd_losses)
 
-        num_categories = nd_losses.shape[1]
+            prediction_train, prediction_test  = model(self.x_train).detach().flatten(), model(self.x_test).detach().flatten()
+            loss_train, loss_test = nn.BCELoss()(prediction_train, self.y_train), nn.BCELoss()(prediction_test, self.y_test)
+            nd_loss_train, categories_train = self.analysis_metric(prediction_train, self.a_train, self.y_train) # categories is the same for all iterations, should be restructured
+            nd_loss_test, categories_test = self.analysis_metric(prediction_test, self.a_test, self.y_test)
+            assert categories_train == categories_test
+            categories = categories_train
+            curr_bottleneck_train, curr_bottleneck_test = ["green"] * len(nd_loss_train), ["blue"] * len(nd_loss_test)
+            curr_bottleneck_train[nd_loss_train.index(max(nd_loss_train))], curr_bottleneck_test[nd_loss_test.index(max(nd_loss_test))] = "red", "orange"
+            bottlenecks_train.append(curr_bottleneck_train)
+            bottlenecks_test.append(curr_bottleneck_test)
+            objective_losses_train.append(loss_train)
+            objective_losses_test.append(loss_test)
+            nd_losses_train.append(nd_loss_train)
+            nd_losses_test.append(nd_loss_test)
+        objective_losses_train, objective_losses_test, nd_losses_train, nd_losses_test = np.array(objective_losses_train), np.array(objective_losses_test), np.array(nd_losses_train), np.array(nd_losses_test)
+
+        num_categories = len(categories)
         plot_dim_1, plot_dim_2 = int(np.floor(np.sqrt(num_categories))), 1
         while num_categories % plot_dim_1 != 0:
             plot_dim_1 -= 1
@@ -92,12 +90,17 @@ class FairnessAwareLearningExperiment:
 
         for i in range(num_categories):
             idx, idy = i // plot_dim_1, i % plot_dim_1
-            axes[idx, idy].scatter(nd_losses[:, i], objective_losses, c=[l[i] for l in bottlenecks])
+            scatter_train = axes[idx, idy].scatter(nd_losses_train[:, i], objective_losses_train,
+                                                   c=[l[i] for l in bottlenecks_train], label='train')
+            scatter_test = axes[idx, idy].scatter(nd_losses_test[:, i], objective_losses_test,
+                                                  c=[l[i] for l in bottlenecks_test], label='test')
+            axes[idx, idy].legend(handles=[scatter_train, scatter_test])
             axes[idx, idy].set_xlabel('Discrimimnatory loss')
             axes[idx, idy].set_ylabel('Objective loss')
             (Y_start, Y_end), (A_start, A_end) = categories[i]
-            category_prob = ((Y_start < Y) & (Y <= Y_end) & (A_start < A) & (A <= A_end)).sum() / len(Y)
-            category_desc = f"Y: ({Y_start:.3f} - {Y_end:.3f}), A: ({A_start:.3f} - {A_end:.3f}), P_ya: {category_prob:.3f}"
+            category_prob_train = ((Y_start < self.y_train) & (self.y_train <= Y_end) & (A_start < self.a_train) & (self.a_train <= A_end)).sum() / len(self.y_train)
+            category_prob_test = ((Y_start < self.y_test) & (self.y_test <= Y_end) & (A_start < self.a_test) & (self.a_test <= A_end)).sum() / len(self.y_test)
+            category_desc = f"Y: ({Y_start:.3f} - {Y_end:.3f}), A: ({A_start:.3f} - {A_end:.3f}), P_ya_train: {category_prob_train:.3f}, P_ya_test: {category_prob_test:.3f}"
             axes[idx, idy].set_title(category_desc, fontsize="xx-small")
 
         fig.suptitle(self.fairness_name)
--- final_comparison_experiment/run.py
+++ final_comparison_experiment/run.py
@@ -13,27 +13,27 @@ if __name__ == "__main__":
     alpha_metric = generate_alpha(intervals, intervals)
     analysis_metric = generate_alpha(intervals, intervals, return_category_names=True)"""
 
-    dataset_name = "uscensus"
+    """dataset_name = "uscensus"
     alpha_intervals = generate_constrained_intervals(9)
-    y_intervals = generate_constrained_intervals(2)
-    beta_metric = generate_beta(alpha_intervals, y_intervals)
-    alpha_metric = generate_alpha(alpha_intervals, y_intervals)
-    analysis_metric = generate_alpha(alpha_intervals, y_intervals, return_category_names=True)
-
-    """dataset_name = "adult"
-    alpha_intervals = generate_constrained_intervals(2)
     y_intervals = generate_constrained_intervals(2)
     beta_metric = generate_beta(alpha_intervals, y_intervals)
     alpha_metric = generate_alpha(alpha_intervals, y_intervals)
     analysis_metric = generate_alpha(alpha_intervals, y_intervals, return_category_names=True)"""
 
+    dataset_name = "adult"
+    alpha_intervals = generate_constrained_intervals(2)
+    y_intervals = generate_constrained_intervals(2)
+    beta_metric = generate_beta(alpha_intervals, y_intervals)
+    alpha_metric = generate_alpha(alpha_intervals, y_intervals)
+    analysis_metric = generate_alpha(alpha_intervals, y_intervals, return_category_names=True)
+
     dataset = read_dataset(dataset_name)
     fairness_weights_beta = np.logspace(np.log10(0.1), np.log10(30), 30)  # TODO: set it based on eta
-    beta_experiment = FairnessAwareLearningExperiment(dataset, beta_metric, "Beta", dataset_name, fairness_weights_beta,
+    beta_experiment = FairnessAwareLearningExperiment(dataset, beta_metric, "Beta_trial", dataset_name, fairness_weights_beta,
                                                       analysis_metric)
     beta_experiment.run_analysis()
 
-    fairness_weights_alpha = np.logspace(np.log10(0.02), np.log10(6), 30)  # TODO: set it based on eta
-    alpha_experiment = FairnessAwareLearningExperiment(dataset, alpha_metric, "Alpha", dataset_name, fairness_weights_alpha,
+    """fairness_weights_alpha = np.logspace(np.log10(0.02), np.log10(6), 30)  # TODO: set it based on eta
+    alpha_experiment = FairnessAwareLearningExperiment(dataset, alpha_metric, "Alpha_trial", dataset_name, fairness_weights_alpha,
                                                        analysis_metric)
-    alpha_experiment.run_analysis()
+    alpha_experiment.run_analysis()"""
