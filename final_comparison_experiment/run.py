c0f5190e538e642df15063c6d163e33032b4556c
varikakasandor <varikakasandor@gmail.com>
2023-10-09 21:22:06 +0200

    Alpha-beta comparison added

--- final_comparison_experiment/run.py
+++ final_comparison_experiment/run.py
@@ -1,12 +1,14 @@
-import numpy as np
+from matplotlib import pyplot as plt
 
-from pipeline import FairnessAwareLearningExperiment
 from datasets import read_dataset
 from fairness_metrics import generate_beta, generate_alpha, generate_constrained_intervals
+from pipeline import FairnessAwareLearningExperiment
+from tools import *
 
 if __name__ == "__main__":
     dataset_name = "adult"
-    real_run = True
+    real_run = False
+    create_comparison = True
 
     if dataset_name == "crimes":
         intervals = generate_constrained_intervals(2)
@@ -45,3 +47,34 @@ if __name__ == "__main__":
     alpha_experiment = FairnessAwareLearningExperiment(dataset, alpha_metric, "Alpha" if real_run else "Alpha_trial", dataset_name,
                                                        fairness_weights_alpha, analysis_metric, num_epochs)
     alpha_results = alpha_experiment.run_analysis()
+
+    if create_comparison:
+        num_categories = len(beta_results.categories)
+        plot_dim_1, plot_dim_2 = find_optimal_subplot_dims(num_categories)
+        fig, axes = plt.subplots(plot_dim_1, plot_dim_2, figsize=(plot_dim_1 * 4, plot_dim_2 * 4))
+
+        for i in range(num_categories):
+            idx, idy = i // plot_dim_1, i % plot_dim_1
+            scatter_beta_train = axes[idx, idy].scatter(beta_results.nd_loss_train[:, i], beta_results.obj_loss_train,
+                                                   c=[l[i] for l in beta_results.bottlenecks_train], label='beta_train')
+            scatter_alpha_train = axes[idx, idy].scatter(alpha_results.nd_loss_train[:, i], alpha_results.obj_loss_train,
+                                                   c=[l[i] for l in alpha_results.bottlenecks_train], label='alpha_train', marker='v')
+            scatter_beta_test = axes[idx, idy].scatter(beta_results.nd_loss_test[:, i], beta_results.obj_loss_test,
+                                                   c=[l[i] for l in beta_results.bottlenecks_test], label='beta_test')
+            scatter_alpha_test = axes[idx, idy].scatter(alpha_results.nd_loss_test[:, i], alpha_results.obj_loss_test,
+                                                   c=[l[i] for l in alpha_results.bottlenecks_test], label='alpha_test', marker='v')
+            axes[idx, idy].legend(handles=[scatter_beta_train, scatter_alpha_train, scatter_beta_test, scatter_alpha_test])
+            axes[idx, idy].set_xlabel('Discrimimnatory loss')
+            axes[idx, idy].set_ylabel('Objective loss')
+            (Y_start, Y_end), (A_start, A_end) = beta_results.categories[i]
+            category_prob_train = ((Y_start < beta_results.y_train) & (beta_results.y_train <= Y_end) & (A_start < beta_results.a_train) & (
+                        beta_results.a_train <= A_end)).sum() / len(beta_results.y_train)
+            category_prob_test = ((Y_start < beta_results.y_test) & (beta_results.y_test <= Y_end) & (A_start < beta_results.a_test) & (
+                        beta_results.a_test <= A_end)).sum() / len(beta_results.y_test)
+            category_desc = f"Y: ({Y_start:.3f} - {Y_end:.3f}), A: ({A_start:.3f} - {A_end:.3f}), P_ya_train: {category_prob_train:.3f}, P_ya_test: {category_prob_test:.3f}"
+            axes[idx, idy].set_title(category_desc, fontsize="xx-small")
+
+        fig.suptitle(f"{beta_results.fairness_name} vs {alpha_results.fairness_name}")
+        plt.tight_layout()
+        plt.savefig(f'./plots/comparison_{beta_results.fairness_name}_{alpha_results.fairness_name}_{beta_results.dataset_name}.pdf')
+        plt.show()
